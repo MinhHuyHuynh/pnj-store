b847f6d9032f9583bb0975798ef59b2d74e51f4a
builethienphuc47 <thienphuc04072001@gmail.com>
2022-05-02 08:55:47 +0700

    builethienphuc

--- public/pnj/images/home/manage_tntt_user.sql
+++
@@ -1,2 +0,0 @@
-insert into manage-tntt.user (id, name, email, password)
-values  (1, 'admin', 'admin@gmail.com', '123456');
--- public/pnj/images/shop/manage_tntt_classify.sql
+++
@@ -1,4 +0,0 @@
-insert into manage-tntt.classify (id, name)
-values  (2, 'kim cương'),
-        (3, 'dây chuyền'),
-        (4, 'áo');
